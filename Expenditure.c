ebe0c1b2333d1f593e6f41131f1e2a200efab473
Nithyasanthoshi <147487660+Nithyasanthoshi@users.noreply.github.com>
2023-10-21 18:06:18 +0530

    #include<stdio.h> int main() {     int X,Y,Z,res;     scanf("%d%d%d",&X,&Y,&Z);     if(X>=30*Y)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y,Z,res;
+    scanf("%d%d%d",&X,&Y,&Z);
+    if(X>=30*Y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
